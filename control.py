a802f45b555b4bbe46029d28955b175dba568d76
frederikhogenbosch98 <fhogenbosch@icloud.com>
2022-11-14 13:18:09 +0100

    init

---
+++ control.py
@@ -0,0 +1,16 @@
+import pymavlink as pmav
+import keyboard as kb
+
+
+PITCH_ANGLE = 0
+ROLL_ANGLE = 0
+YAW_ANGLE = 0
+
+def handleLeftKey(e):
+    if kb.is_pressed("4"):
+        print("left arrow was pressed w/ key 4")
+        # work your magic
+
+while True:
+
+    kb.on_press_key("left", handleLeftKey)s
