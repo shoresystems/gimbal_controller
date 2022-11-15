e63ce861575bdc4552a9d470b300ac2ef14a82a5
frederikhogenbosch98 <fhogenbosch@icloud.com>
2022-11-15 12:00:58 +0100

    yaw

--- control.py
+++ control.py
@@ -1,16 +1,18 @@
 import pymavlink as pmav
 import keyboard as kb
-
+import time
 
 PITCH_ANGLE = 0
 ROLL_ANGLE = 0
 YAW_ANGLE = 0
 
+
 def handleLeftKey(e):
-    if kb.is_pressed("4"):
-        print("left arrow was pressed w/ key 4")
-        # work your magic
+    YAW_ANGLE += 10
+    return YAW_ANGLE
 
 while True:
 
-    kb.on_press_key("left", handleLeftKey)s
+    kb.onpress_key("left", handleLeftKey)
+    print('yaw', YAW_ANGLE)
+    time.sleep(0.1)
