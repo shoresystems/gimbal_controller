58eb349e22e9c1e137fdf8e8673cdf8089fa3d4b
frederikhogenbosch98 <fhogenbosch@icloud.com>
2022-11-15 15:07:06 +0100

    basic funcs

--- control.py
+++ control.py
@@ -1,4 +1,4 @@
-import pymavlink as pmav
+from pymavlink import mavutil
 import keyboard as kb
 import time
 
@@ -6,14 +6,72 @@ PITCH_ANGLE = 0
 ROLL_ANGLE = 0
 YAW_ANGLE = 0
 
+MAX_ANGLE = 180
 
-def handleLeftKey():
-    YAW_ANGLE += 10
-    return YAW_ANGLE
+
+def handleLeftKey(ROLL_ANGLE):
+    return max(ROLL_ANGLE - 10, -MAX_ANGLE)
+
+def handleRightKey(ROLL_ANGLE):
+    return min(ROLL_ANGLE + 10, MAX_ANGLE)
+
+def handleWKey(PITCH_ANGLE):
+    return min(PITCH_ANGLE + 10, MAX_ANGLE)
+
+def handleSKey(PITCH_ANGLE):
+    return max(PITCH_ANGLE - 10, -MAX_ANGLE)
+
+def handleAKey(YAW_ANGLE):
+    return max(YAW_ANGLE - 10, -MAX_ANGLE)
+
+def handleDKey(YAW_ANGLE):
+    return min(YAW_ANGLE + 10, MAX_ANGLE)
+
+def move_gimbal(PITCH_ANGLE, ROLL_ANGLE=0, YAW_ANGLE=0):
+    """
+    Moves gimbal to given position
+    Args:
+        PITCH_ANGLE (float): tilt angle in centidegrees (0 is forward)
+        ROLL_ANGLE (float, optional): pan angle in centidegrees (0 is forward)
+        YAW_ANGLE  (float, optional): pan angle in centidegrees (0 is forward)
+    """
+    master.mav.command_long_send(
+        master.target_system,
+        master.target_component,
+        mavutil.mavlink.MAV_CMD_DO_MOUNT_CONTROL,
+        1,
+        PITCH_ANGLE,
+        ROLL_ANGLE,
+        YAW_ANGLE,
+        0, 0, 0,
+        mavutil.mavlink.MAV_MOUNT_MODE_MAVLINK_TARGETING)
+
+
+
+devloc = 'udpin:0.0.0.0:14550'
+print('connecting to device on: ', devloc)
+master = mavutil.mavlink_connection(devloc)
+master.wait_heartbeat()
+print('heartbeat found')
+
+
 
 while True:
-
     if kb.is_pressed("left"):
-        handleLeftKey()
-    print('yaw', YAW_ANGLE)
-    time.leep(0.1)
+        ROLL_ANGLE = handleLeftKey(ROLL_ANGLE)
+    elif kb.is_pressed("right"):
+        ROLL_ANGLE = handleRightKey(ROLL_ANGLE)
+    elif kb.is_pressed('w'):
+        PITCH_ANGLE = handleWKey(PITCH_ANGLE)
+    elif kb.is_pressed("s"):
+        PITCH_ANGLE = handleSKey(PITCH_ANGLE)
+    elif kb.is_pressed("a"):
+        YAW_ANGLE = handleAKey(YAW_ANGLE)
+    elif kb.is_pressed("d"):
+        YAW_ANGLE = handleDKey(YAW_ANGLE)
+    else:
+        continue
+    print("")
+    move_gimbal(PITCH_ANGLE, ROLL_ANGLE, YAW_ANGLE)
+    print('pitch: ', PITCH_ANGLE, 'roll: ', ROLL_ANGLE, 'yaw: ', YAW_ANGLE)
+    time.sleep(0.1)
